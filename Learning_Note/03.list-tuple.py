bf13bfcccd1895440071212456c31df918857afa
Teddy <ssy2216ated@gmail.com>
2022-03-09 16:06:10 +0800

    OK

--- Learning_Note/03.list-tuple.py
+++ Learning_Note/03.list-tuple.py
@@ -26,6 +26,8 @@ grades=[12,60,15]
 grades=grades+[11,33]
 print(grades)
 '''
+
+
 data=(3,4,5)
 print(data[0:2])
 #data=[0]=5 #錯誤：Tuple的資料不可變動
@@ -45,6 +47,7 @@ print(data[0:2])
 #當你要建立只有一個項目的 tuple，千萬要記得加上逗號，否則不會建立 tuple。
 
 '-----------------------------------------------------------------------------------'
+
 #列表學習
 
 a = [1,2,3]
@@ -52,4 +55,108 @@ b = [1 , 'abc' , 2.0 , ['a', 'b' , 'c']]
 
 print(a)
 print(b)
-print(a[0])
+print(b[3][0], a[0], end = "\n", sep = "-") #按下control然後再按print，會看到print的function，end後面預設是接"/n"；然後sep是分割符號，預設是空白。
+
+c = b[1:3]
+
+print(c, type(c))
+
+s = 'abcdef'
+
+print(s[1:3], s[-1])
+
+print(b[-1])  #-1代表的是最後一個數字，index都從0開始。
+
+'-----------------------------------------------------------------------------------'
+
+#list的操作
+
+list_1 = [9, -1, -4, 3, 7, 11, 3]
+
+print("List_1的長度=", len(list_1)) #List的長度
+
+print("List的最大值=", max(list_1)) #找最大值
+
+print("List裡的最小值=", min(list_1)) #找最小值
+
+print("List裡'3'這個元素出現過{}次".format(list_1.count(3)))
+print(f"List裡'3'這個元素出現過{list_1.count(3)}次") #兩種字串格式化的方法，都是透過 list.count(x) 來作計數
+
+#List的改動
+list_2 = ["a",'b',"c","d"]
+
+#給List結尾加一個新元素
+list_2.append("e")
+print("list_2=", list_2)
+
+#在list2的a、c加入一個b元素，在某個index"前"面插入某個參數
+list_2.insert(1,'b')
+print("list_2=", list_2)
+
+#刪除列表裡的某個參數
+list_2.remove("b")
+print("list_2=", list_2)  #如果有重複參數需要刪除，可以使用while迴圈如下
+"----------------------------------------------"
+target = 'dog'
+list = ['cat', 'dog', 'dog', 'pig', 'dog']
+while(target in list):
+    list.remove(target)
+"----------------------------------------------"
+
+list_2.pop(1) #刪除特定index位置的元素，或者使用del list[x]
+print(list_2)
+
+del list_2[0:2] #代表刪除從 0,1 index的資料，2的index元素則不會影響。
+print(list_2)
+
+
+#修改index所代表的值
+list_2[0] = "1"
+print(list_2)
+
+#如果是字串，沒有辦法跟列表一樣進行修改，只能整組替換
+#a = '123'
+#a[0] = 'a' 會跳錯
+#a = 'abc'
+
+#列表翻轉
+
+list_3 = [1,2,3]
+list_3.reverse() #排序反轉
+print(list_3)
+
+
+list_4 = [9, -1, -4, 3, 7, 11, 3]
+list_4.sort(reverse = False,key = int) #排序，如果加reverse也可以，若為True則由大到小，False則由小到大。
+print(list_4)
+
+list_5 = [1,0,-5,-7]
+list_5.sort()
+print(list_5)
+
+"---------------------------------------------------------------"
+#Tuple 元組
+
+a = (1,2,3)   #Tuple的寫法
+b = 1,        #Tuple的寫法
+print(type(a))
+print(type(b))
+
+print(a[1])
+print(a[1:])
+print(a[-1])
+
+
+#Tuple的改變?
+tuple1 = ('a','b','c')
+#tuple1[0] = 2   無法操作，代表排序、翻轉、抽換都不行
+
+print(tuple1.index('b')) #去找在Tuple內的值，是第幾個Index。
+
+#List v.s Tuple
+'Tuple是不可變的Python的對象，只需要固定的內存，immutable的'
+
+'List是可變的，mutable，python對象，需要兩個內存空間，一個用來儲存實際的列表數據，一塊是可變動的空間用於擴展。'
+#Tuple創建和訪問要比列表還要快，但是不如列表靈活。
+
+#若數據量小，使用List或者Tuple都無所謂，但若是絕對不允許改變，則可以使用Tuple。
--- Learning_Note/16.裝飾器_decorator.py
+++ Learning_Note/16.裝飾器_decorator.py
@@ -24,20 +24,74 @@ def 函式名稱():
 函式名稱() #呼叫帶有裝飾器的函式。
 '''
 
-def testDecorator(callback):
-    def innerFunc():
-        print("裝飾器函式")
-        callback()
-    return innerFunc
+#def testDecorator(callback):
+#    def innerFunc():
+#        print("裝飾器函式")
+#        callback()
+#    return innerFunc
 
 #@testDecorator
-def decoratedFunc():
-    print("普通函式")
+#def decoratedFunc():
+#    print("普通函式")
+
+#有了裝飾器後，我們怎麼運用函式來工作？
+
+from tkinter import N
+
+
+def testDecorator(callback):
+    def innerFunc(inner_data):
+        print("裝飾器函式")
+        print(inner_data)
+        callback("裡面的程式碼")    #callback才是真正對應到下方decorated，所以下方函式想要帶參數，就要放在這邊的Callback
+    return innerFunc
+
+@testDecorator
+def decoratedFunc(data):
+    print("普通函式"+"加上", data)
+
+decoratedFunc("Inner_Func的資料")  #一般來說，一般函式我們要加個參數(Parameter)進去，我們直接在decoratedFunc裡面加參數就好。若要使用裝飾器，就必須先呼叫Callback，
 
 #如果不用@這類型的語法糖，就要撰寫如下方:
-testDecorator(decoratedFunc)()
+#testDecorator(decoratedFunc)("Inner_Func的資料")
 
 #重要!裝飾器其實是為了避免重複勞動，因為當有類似的功能要執行的時候，不必每次都寫一次。
 #此外，請注意，testDecorator這個裝飾器函式，裡面包的inner函式，才是我們要執行的。
 
 #也就是說，testDecorator(decoratedFunc)，實質上會先把decoratedFunc(做為callback)丟進testDecorator，接著被整併進innerFunc，然後整個testDecorator會回給你一個把decoratedFunc包進去的整包函式(也就是innerFunc)，接著記得再用一個()，因為函式回傳的是另一個函式。
+
+
+def calculate(callback):
+    def run():                #----------------
+        result = 0
+        for n in range(51):   #這是我們想讓裝飾器作的事情。
+            result += n
+        print (result)        #----------------
+        callback()            #我們下方的主要函式
+    return run
+
+@calculate
+def show():
+    print("普通函式的程式碼")
+
+#實際應用，我想重複運用1加到50的運算功能，我就可以用裝飾器。
+def calculate(callback):
+    def run():
+        #裝飾器想要執行的程式碼
+        result = 0
+        for n in range(51):
+            result += n
+        #把計算結果透過參數傳到被裝飾的普通函式中。
+        callback(result)
+    return run
+
+@calculate
+def show(n):
+    print("計算結果是", n)
+
+@calculate
+def showEnglish(n):
+    print("Result is", n)
+
+show()
+showEnglish()
